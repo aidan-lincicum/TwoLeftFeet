524e6ad7ef3c9b7319fb428b081e8cb9d5d8c095
Aidan Lincicum <aidanlincicum@gmail.com>
2024-02-20 16:36:47 -0600

    Fixed sliding issue and made it so tank only turns left.

--- Assets/player.gd
+++ Assets/player.gd
@@ -4,10 +4,11 @@ extends "res://Assets/tank.gd"
 func player_input(delta):
 	var dir = 0
 	if Input.is_action_pressed('turn_right'):
-		dir += 1
+		dir += 0
 	if Input.is_action_pressed('turn_left'):
 		dir -= 1
 	rotation += rotation_speed * dir * delta
+	velocity = Vector2(0,0).rotated(rotation_speed)
 	if Input.is_action_pressed('forward'):
 		velocity = Vector2(speed, 0).rotated(rotation)
 	if Input.is_action_pressed('back'):
