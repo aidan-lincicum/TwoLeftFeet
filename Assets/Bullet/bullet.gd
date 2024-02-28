bfedbe4e15bfd57e0a055bdba61b8f218fea29f8
QuinnTange <118770255+QuinnTange@users.noreply.github.com>
2024-02-27 18:09:08 -0600

    added ricochet *TIMER BROKEN*
    bullet changed from Area2d to characterbody2D, this broke the timer and some other tsuff, but we fixed the other broken stuff

--- Assets/Bullet/bullet.gd
+++ Assets/Bullet/bullet.gd
@@ -1,25 +1,40 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed : int
 @export var timer : float
 
-var velocity = Vector2()
+#var velocity = Vector2()
 
 func start(_position, _direction):
 	position = _position
 	rotation = _direction.angle()
-	$Timer.wait_time = timer
+	$Timer.wait_time = Timer
 	velocity = _direction * speed
+	var timer = get_node("Timer")
+	timer.timeout.connect(on_timer_timeout)
 	
-func _process(delta):
-	position += velocity * delta
 
-func _on_Timer_timeout():
+func on_timer_timeout():
 	queue_free()
+
+func _process(delta):
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	
-func _on_body_entered(area):
-	if area.name.begins_with("Player"):
-		queue_free()
+	if collision:
+		var colliding_name = collision.get_collider().get("name")
+		if colliding_name.begins_with("Player"):
+			hit() 
+		else:
+			var reflect = collision.get_remainder().bounce(collision.get_normal())
+			velocity = velocity.bounce(collision.get_normal())
+			move_and_collide(reflect)
+
+
+	
+func hit():
+	queue_free()
+#func _on_body_entered(area):
+	
 	#if area.name == "StaticBody2D":
 		#ricochet()
 
