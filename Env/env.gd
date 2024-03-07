85b63bdcdbd8f498814f294ca2350ad29bdf96b9
QuinnTange <118770255+QuinnTange@users.noreply.github.com>
2024-03-07 13:00:24 -0600

    power ups show up on screen random timer

--- Env/env.gd
+++ Env/env.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var power_up: PackedScene
 
 var power_timer_is_running = false
+var rng = RandomNumberGenerator.new()
+var power_up_x_position
+var power_up_y_position
 
 func _on_Tank_shoot(bullet, _position, _direction):
 	var b = bullet.instantiate()
@@ -10,29 +13,27 @@ func _on_Tank_shoot(bullet, _position, _direction):
 	b.start(_position, _direction)
 
 func _process(delta):
-	var rng = RandomNumberGenerator.new()
 	if(!power_timer_is_running):
 		var my_random_number = rng.randf_range(0, 1)
 		makeTimer(my_random_number)
 		
 func _ready():
-	var rng = RandomNumberGenerator.new()
-	var my_random_number = rng.randf_range(0, 1)
-	makeTimer(my_random_number)
-	var timer = get_node("powerUpTimer")
+	var random_time_amount = rng.randf_range(0, 1)
+	makeTimer(random_time_amount)
 	
 func makeTimer(amount_time):
 	$powerUpTimer.wait_time = amount_time
-	print($powerUpTimer.wait_time)
-	$powerUpTimer.timeout.connect(_on_timer_timeout)
 	power_timer_is_running = true
 
 func spawn_power_up():
-	print("spawnPowerUP")
-	var power_up = power_up.instantiate()
-	power_up.start()
+	#print("spawnPowerUP")
+	var power_up_instance = power_up.instantiate()
+	add_child(power_up_instance)
+	power_up_x_position = rng.randf_range(-500,500)
+	power_up_y_position = rng.randf_range(-350,350)
+	power_up_instance.position = Vector2(power_up_x_position,power_up_y_position)
 	
-func _on_timer_timeout():
+func _on_power_up_timer_timeout():
 	print("timer timout")
 	spawn_power_up()
 	power_timer_is_running = false
---
+++ Env/power_up.gd
@@ -0,0 +1,9 @@
+extends Area2D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func start():
+	print("raedy")
+	position = Vector2(0,0)
+
