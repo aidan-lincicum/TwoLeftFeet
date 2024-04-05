ffcab49a8d79a06c375994e7a79b98f9b8c1339a
QuinnTange <118770255+QuinnTange@users.noreply.github.com>
2024-04-05 15:13:41 -0500

    power ups are only health packs

--- Env/power_up.gd
+++ Env/power_up.gd
@@ -6,19 +6,9 @@ var rng = RandomNumberGenerator.new()
 func _process(_delta):
 	pass
 	
-func start():
-	print("raedy")
-	position = Vector2(0,0)
-	
 func _on_body_entered(area):
 	if area.name.begins_with("Player"):
-		var my_random_number = rng.randi_range(1, 3)
-		if(my_random_number == 1):
-			area.get_power_up(800,"speed")
-		if(my_random_number == 2):
-			area.get_power_up(0.1,"turret_cd")
-		if(my_random_number == 3):
-			area.get_power_up(3,"max_hearts")
+		area.get_power_up(100,"max_hearts")
 		queue_free()
 	if area.name.begins_with("StaticBody2D"):
 		queue_free()
